2dfd84354b3a9c0b3a962bb175f7cb5e70a2d7e5
Abriel <106871954+Abrielz1@users.noreply.github.com>
2022-08-04 19:26:47 +0300

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Calculator {
+
+    private Calculator() {}
+
+   public static double calculate(List<MediaItem> mediaItems) {
+            int items = 0;
+        for (MediaItem item : mediaItems){
+        if (item instanceof Movie){
+            Movie movie = (Movie) item;
+            items += movie.getRuntime();
+        }  else if (item instanceof Series){
+            Series series = (Series) item;
+               items += item.getRuntime() * series.getSeriesCount();
+           }
+        }
+        return ((double) items) / 60 / 24;
+    }
+}
